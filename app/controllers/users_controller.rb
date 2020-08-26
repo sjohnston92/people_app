9ba6938048e8cb636a2f45c4d9b071f30ce43651
sjohnston92 <simonjohnston.c@gmail.com>
2020-08-25 18:09:56 -0600

    WOOOOOOO DONE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,16 +12,35 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user = User.find(params[:id]).destroy
+    redirect_to users_path
   end
 
   private 
+  def user_params
+  params.require(:user).permit(:first_name, :last_name, :age, :hair_color)
+  end
 end
